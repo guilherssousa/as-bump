293de7a13c17ab5ad1b8bc4b49fae9a891f878fe
Guilherme Sousa <guilherssousa@gmail.com>
2023-01-29 12:01:38 -0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { useState } from "react";
-
-import "./styles/App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <h1>[adult swim bump generator]</h1>
-
-      <p>Because now you can make you own bumper.</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h1>[adult swim bump generator]</h1>
+        <p>Because everyone wants to mock [as] aesthetic.</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import "./index.css";
+
+const root = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
