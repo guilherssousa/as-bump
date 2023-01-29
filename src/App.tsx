e1328395dde290583afc87c413e4647b65cd4665
Guilherme Sousa <guilherssousa@gmail.com>
2023-01-29 12:20:44 -0300

    [ft] start working on some spicies

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,72 @@ import Layout from "./components/Layout";
 function App() {
   return (
     <Layout>
-      <div>
-        <h1>[adult swim bump generator]</h1>
-        <p>Because everyone wants to mock [as] aesthetic.</p>
-      </div>
+      <h1 className="text-5xl font-semibold">[adult swim bump generator]</h1>
+      <p className="mt-4 text-xl">
+        Because everyone wants to mock [as] aesthetic.
+      </p>
+
+      <section className="mt-16">
+        <div className="text-center">
+          <h2 className="text-4xl font-semibold">What's a bump?</h2>
+          <blockquote
+            cite="Someone at [as]"
+            className="text-lg mt-2 text-neutral-400 font-normal"
+          >
+            Are you lurking the internet for coming here and not knowing what a
+            bumper is? ~ [as] staff
+          </blockquote>
+        </div>
+
+        <div className="text-lg mt-4 text-justify text-neutral-200">
+          <p>
+            In broadcasting, a commercial bumper, ident bumper, or break-bumper
+            (often shortened to bump){" "}
+            <span className="font-semibold text-white">
+              {" "}
+              is a brief announcement, usually two to fifteen seconds in length
+              that can contain a voice over, placed between a pause in the
+              program and its commercial break,and vice versa.
+            </span>{" "}
+            The host, the program announcer, or a continuity announcer states
+            the title (if any) of the presentation, the name of the program, and
+            the broadcast or cable network, though not necessarily in that
+            order. On children's television networks, they are sometimes called
+            external eyecatches due to the resemblance of internal eyecatches in
+            anime and there is usually no voice over, but some bumpers do
+            feature one. Bumper music, often a recurring signature or theme
+            music segment, is nearly always featured. Bumpers can vary from
+            simple text to short films.{" "}
+            <span className="font-semibold text-white">
+              <a href="https://en.wikipedia.org/wiki/Bumper_(broadcasting)">
+                [source: Wikipedia]
+              </a>
+            </span>
+          </p>
+
+          <p className="mt-4">
+            The Adult Swim Bumpers were originally shots of senior citizens
+            swimming and doing random pool-related activities in public pools.
+            The lifeguard is often heard talking through a megaphone and saying
+            things such as "Adult Swim, all kids out of the pool!", and
+            announcing the forthcoming TV show. The logo was, basically, "ADULT
+            SWIM" written in large red/or yellow capital letters (or a alternate
+            version of the logo featured "adult swim" in red lowercase letters
+            in a black circle with a yellow penumbra, but sometimes the letters
+            can be rarely in capital). On Saturdays for the Action Block
+            starting February 23, 2002, the shows were announced by a computer
+            generated voice and the bumps featured clips of the shows. Adult
+            Swim's main theme music was "D-Code", performed by Melbourne artist
+            Dust Devil. The song was a remix of "Mambo Gallego" by Tito Puente,
+            Sr.{" "}
+            <span className="font-semibold text-white">
+              <a href="https://adultswim.fandom.com/wiki/Bumps">
+                [source: Adult Swim Wiki]
+              </a>
+            </span>
+          </p>
+        </div>
+      </section>
     </Layout>
   );
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,19 @@ import { HTMLAttributes } from "react";
 type Props = HTMLAttributes<HTMLElement>;
 
 function Layout({ children, ...props }: Props) {
-  return <main {...props}>{children}</main>;
+  return (
+    <main
+      {...props}
+      className={"min-h-screen bg-black text-white".concat(
+        " ",
+        props?.className ?? ""
+      )}
+    >
+      <div className="max-w-screen-lg mx-auto pt-16 flex items-center flex-col">
+        {children}
+      </div>
+    </main>
+  );
 }
 
 export default Layout;
